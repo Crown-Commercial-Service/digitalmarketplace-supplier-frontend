b16409f5c0771d3d4440b5152971e9659fa23eb9
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-02-27 17:23:48 +0000

    Update to run on port 5003
    For development we will want to run multiple apps, so they should each bind to a different port number.

--- application.py
+++ application.py
@@ -2,10 +2,11 @@
 
 import os
 from app import create_app
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Server
 
 application = create_app(os.getenv('FLASH_CONFIG') or 'default')
 manager = Manager(application)
+manager.add_command("runserver", Server(port=5003))
 
 if __name__ == '__main__':
     manager.run()
