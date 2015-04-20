51c9413eb1375ff191e03d38933a772923fa55cf
Alexey Bezhan <al@fueledbylemons.com>
2015-04-20 11:53:42 +0100

    Add /supplier prefix to main blueprint and static URLs

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,23 @@ from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from config import config
 
-
-bootstrap = Bootstrap()
+from .main import main as main_blueprint
 
 
 def create_app(config_name):
 
-    application = Flask(__name__)
-    application.config['DEBUG'] = True
+    application = Flask(__name__,
+                        static_folder='static/',
+                        static_url_path=config[config_name].STATIC_URL_PATH)
+
     application.config.from_object(config[config_name])
     config[config_name].init_app(application)
 
+    bootstrap = Bootstrap()
     bootstrap.init_app(application)
 
-    from .main import main as main_blueprint
-    application.register_blueprint(main_blueprint)
+    application.register_blueprint(main_blueprint,
+                                   url_prefix='/supplier')
     main_blueprint.config = {
         'BASE_TEMPLATE_DATA': application.config['BASE_TEMPLATE_DATA']
     }
