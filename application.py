08558ec179e24908d5602c1dae08f796abab211c
Alexey Bezhan <al@fueledbylemons.com>
2015-04-20 15:08:28 +0100

    Rename config environments to match deployment stages

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from app import create_app
 from flask.ext.script import Manager, Server
 
 application = create_app(
-    os.getenv('DM_SUPPLIER_FRONTEND_ENVIRONMENT') or 'default'
+    os.getenv('DM_ENVIRONMENT') or 'development'
 )
 manager = Manager(application)
 manager.add_command("runserver", Server(port=5003))
--- config.py
+++ config.py
@@ -34,8 +34,9 @@ class Live(Config):
 
 
 config = {
-    'live': Live,
     'development': Development,
+    'preview': Development,
+    'staging': Live,
+    'production': Live,
     'test': Test,
-    'default': Development
 }
