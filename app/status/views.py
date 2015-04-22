e1f7513b7c0ae10e8fcfbb43db093c4e61b809d5
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-04-22 13:48:24 +0100

    updated the readme with the npm commands from buyer frontend. Run the npm commands.

--- app/status/views.py
+++ app/status/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, current_app
 
 from . import status
 from . import utils
@@ -6,4 +6,6 @@ from . import utils
 
 @status.route('/_status')
 def status():
+    for rule in current_app.url_map.iter_rules():
+        print rule
     return jsonify(status="ok", version=utils.get_version_label())
