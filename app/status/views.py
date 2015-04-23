7dd00b1aa92c043555f95aa506a77822fb7c3a52
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-04-23 17:15:00 +0100

    Removed print stateent

--- app/status/views.py
+++ app/status/views.py
@@ -6,6 +6,4 @@ from . import utils
 
 @status.route('/_status')
 def status():
-    for rule in current_app.url_map.iter_rules():
-        print rule
     return jsonify(status="ok", version=utils.get_version_label())
