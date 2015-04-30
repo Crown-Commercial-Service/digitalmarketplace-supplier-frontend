ddc73ffe5f68d57afc8965078fcdc8f61aaf589d
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-04-30 17:20:47 +0100

    Move test to it's own file
    The `from_json` method being tested has moved from the api client to the model,
    so I've moved the test to reflect the change.

---
+++ tests/app/test_model.py
@@ -0,0 +1,18 @@
+from app import create_app, ApiClient
+from .flask_api_client.test_api_client import TestApiClient
+from .helpers import BaseApplicationTest
+
+from app.model import User
+from nose.tools import assert_equal
+
+
+class TestModel(BaseApplicationTest):
+
+    def __init__(self):
+        self.app = create_app('test')
+        self.api_client = ApiClient(self.app)
+
+    def test_user_from_json(self):
+        result = User.from_json(TestApiClient.user())
+        assert_equal(result.id, 987)
+        assert_equal(result.email_address, 'email_address')
