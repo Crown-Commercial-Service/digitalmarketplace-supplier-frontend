1340f06e73cbd78c8aa779d1b909192ec6a76559
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-05-06 17:23:47 +0100

    Pull out `_status` error handling into dmutils
    API Client now handles exceptions and errors, meaning
    that _status routes for our frontend apps need less logic.

--- tests/app/status/test_status.py
+++ tests/app/status/test_status.py
@@ -1,29 +1,17 @@
 import json
 from ..helpers import BaseApplicationTest
-from requests import Response
 
 import mock
 from nose.tools import assert_equal, assert_in
 
 
 class TestStatus(BaseApplicationTest):
-    def setup(self):
-        super(TestStatus, self).setup()
 
-        self._api_response = mock.patch(
-            'app.status.utils.return_response_from_api_status_call',
-        ).start()
-
-    def teardown(self):
-        self._api_response.stop()
-
-    def test_status_ok(self):
-        api_response = Response()
-        api_response.status_code = 200
-        api_response._content = json.dumps({
-            'status': 'ok'
-        }).encode('utf-8')
-        self._api_response.return_value = api_response
+    @mock.patch('app.status.views.data_api_client')
+    def test_status_ok(self, data_api_client):
+        data_api_client.get_status.return_value = {
+            "status": "ok"
+        }
 
         status_response = self.client.get('/_status')
         assert_equal(200, status_response.status_code)
@@ -34,13 +22,14 @@ class TestStatus(BaseApplicationTest):
         assert_equal(
             "ok", "{}".format(json_data['api_status']['status']))
 
-    def test_status_api_responses_return_500(self):
-        api_response = Response()
-        api_response.status_code = 500
-        api_response._content = json.dumps({
-            'status': 'error'
-        }).encode('utf-8')
-        self._api_response.return_value = api_response
+    @mock.patch('app.status.views.data_api_client')
+    def test_status_error(self, data_api_client):
+
+        data_api_client.get_status.return_value = {
+            'status': 'error',
+            'app_version': None,
+            'message': 'Cannot connect to (Data) API'
+        }
 
         status_response = self.client.get('/_status')
         assert_equal(500, status_response.status_code)
@@ -52,15 +41,3 @@ class TestStatus(BaseApplicationTest):
             "error", "{}".format(json_data['api_status']['status']))
         assert_in(
             "Error connecting to", "{}".format(json_data['message']))
-
-    def test_status_api_responses_are_none(self):
-        self._api_response.return_value = None
-
-        status_response = self.client.get('/_status')
-        assert_equal(500, status_response.status_code)
-
-        json_data = json.loads(status_response.get_data().decode('utf-8'))
-        assert_equal(
-            "error", "{}".format(json_data['status']))
-        assert_equal(None, json_data['api_status'])
-        assert_in("Error connecting to", "{}".format(json_data['message']))
