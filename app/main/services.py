aecc7426f1c1093a656ffbc2b3b219d977a9ecb1
Chris Hill-Scott <me@quis.cc>
2015-05-07 16:00:11 +0100

    Tidy up URLs

--- app/main/services.py
+++ app/main/services.py
@@ -4,7 +4,8 @@ from flask import render_template
 from app.main import main
 from .. import data_api_client
 
-@main.route('/dashboard')
+
+@main.route('/')
 @login_required
 def dashboard():
     template_data = main.config['BASE_TEMPLATE_DATA']
@@ -18,12 +19,12 @@ def dashboard():
         **template_data), 200
 
 
-@main.route('/services/<string:service_id>')
+@main.route('/service/<string:service_id>')
 @login_required
 def services(service_id):
     template_data = main.config['BASE_TEMPLATE_DATA']
     return render_template(
-        "services/services.html",
+        "services/service.html",
         service_id=service_id,
         service_data=data_api_client.get_service(service_id),
         **template_data), 200
