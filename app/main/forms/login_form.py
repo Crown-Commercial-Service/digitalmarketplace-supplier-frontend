656a172009f375f4bcf44b76c1a26af69e3e4e9d
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-05-11 13:57:48 +0100

    Add specific validation messages to login form
    Currently the login form has a message like "There are problems with the following fields:...",
    but it doesn't say what the problems were.  This adds some error messages that explain why the form
    did not validate.

--- app/main/forms/login_form.py
+++ app/main/forms/login_form.py
@@ -4,9 +4,11 @@ from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(Form):
-    email_address = StringField('Email address',
-                                validators=[DataRequired(), Email()])
-    password = PasswordField('Password',
-                             validators=[
-                                 DataRequired()
-                             ])
+    email_address = StringField('Email address', validators=[
+        DataRequired(message="Email can not be empty"),
+        Email(message="Please enter a valid email address")
+    ])
+
+    password = PasswordField('Password', validators=[
+        DataRequired(message="Please enter your password")
+    ])
