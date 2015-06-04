481b1b52334bc00057b781908cecf17494cecb11
Alexey Bezhan <al@fueledbylemons.com>
2015-06-04 10:17:21 +0100

    Add list-entry and word-counter to static files
    Adds application.js to the build process and fixes frontend-toolkit
    bower path not matching the .js include paths.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,8 +6,13 @@
 */
 //= include ../../../node_modules/govuk_frontend_toolkit/javascripts/vendor/polyfills/bind.js
 //= include ../../../bower_components/jquery/dist/jquery.js
+//= include ../../../bower_components/hogan/web/builds/3.0.2/hogan-3.0.2.js
+//= include ../../../bower_components/digitalmarketplace-frontend-toolkit/toolkit/javascripts/list-entry.js
+//= include ../../../bower_components/digitalmarketplace-frontend-toolkit/toolkit/javascripts/word-counter.js
 //= include ../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/selection-buttons.js
 //= include _selection-buttons.js
+//= include _analytics.js
+
 (function(GOVUK, GDM) {
 
   "use strict";
