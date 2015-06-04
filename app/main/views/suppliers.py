62d049145d100507f777305e72a8533a644c8d5f
Alexey Bezhan <al@fueledbylemons.com>
2015-06-04 11:56:26 +0100

    Add supplier info edit page
    Allows editing supplier and main contact details.
    There's no validation apart from form field limits, data is sent
    to the API and the response "error" message is displayed if the
    request fails or returns 400.
    
    Things that are missing compared to dm-prototype design:
    * Last edit date, since it's not returned by the API yet

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -25,3 +25,54 @@ def dashboard():
         supplier=supplier,
         **template_data
     ), 200
+
+
+@main.route('/edit', methods=['GET'])
+@login_required
+def edit_supplier(error=None):
+    template_data = main.config['BASE_TEMPLATE_DATA']
+
+    try:
+        supplier = data_api_client.get_supplier(
+            current_user.supplier_id
+        )['suppliers']
+        supplier['contact'] = supplier['contactInformation'][0]
+    except APIError as e:
+        abort(e.status_code)
+
+    return render_template(
+        "suppliers/edit_supplier.html",
+        supplier=supplier,
+        error=error,
+        **template_data
+    ), 200
+
+
+@main.route('/edit', methods=['POST'])
+@login_required
+def update_supplier():
+    try:
+        data_api_client.update_supplier(
+            current_user.supplier_id,
+            {
+                "description": request.form['description'],
+                "clients": request.form.getlist('clients'),
+            },
+            current_user.email_address,
+            "Update supplier info"
+        )
+
+        data_api_client.update_contact_information(
+            current_user.supplier_id,
+            request.form['contactId'],
+            {k: request.form[k] for k in [
+                "address1", "address2", "city", "country", "postcode",
+                "website", "phoneNumber", "email", "contactName",
+            ]},
+            current_user.email_address,
+            "Update supplier info"
+        )
+    except APIError as e:
+        return edit_supplier(error=e.message)
+
+    return redirect(url_for(".dashboard"))
