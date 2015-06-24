41ced8283ebd9e8ff9bca15d3c61fdfd2171a68a
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-06-24 13:29:29 +0100

    Integrate with data API
    This includes creating a custom wtforms boolean field that supports
    being not set. This is because the boolean field in the toolkit has a
    radio button for true and one for false. However, it can also be true
    that neither of them are checked. This may need to move into utils or
    the toolkit.
    
    The form is not confirmed and will likely change.

--- app/main/views/frameworks.py
+++ app/main/views/frameworks.py
@@ -1,9 +1,12 @@
-from flask import render_template
-from flask_login import login_required
+from flask import render_template, request, abort, flash
+from flask_login import login_required, current_user
 
+from dmutils.apiclient import APIError
 from dmutils import flask_featureflags
 
 from ...main import main
+from ... import data_api_client
+from ..forms.frameworks import G7SelectionQuestions
 
 
 @main.route('/frameworks/g-cloud-7', methods=['GET'])
@@ -21,15 +24,43 @@ def framework_dashboard():
     ), 200
 
 
-@main.route('/frameworks/g-cloud-7/pre-qualification-questionnaire',
+@main.route('/frameworks/g-cloud-7/selection-questions',
             methods=['GET', 'POST'])
 @login_required
 @flask_featureflags.is_active_feature('GCLOUD7_OPEN')
-def framework_pqq():
+def framework_selection_questions():
     template_data = main.config['BASE_TEMPLATE_DATA']
 
+    if request.method == 'POST':
+        form = G7SelectionQuestions()
+        if form.validate_on_submit():
+            try:
+                data_api_client.answer_selection_questions(
+                    current_user.supplier_id,
+                    'g-cloud-7',
+                    form.data,
+                    current_user.email_address
+                )
+                flash('questions_updated')
+            except APIError as e:
+                abort(e.status_code)
+    else:
+        try:
+            response = data_api_client.get_selection_answers(
+                current_user.supplier_id, 'g-cloud-7')
+            answers = response['selectionAnswers']['questionAnswers']
+        except APIError as e:
+            if e.status_code != 404:
+                abort(e.status_code)
+            answers = {}
+        form = G7SelectionQuestions(formdata=None, data=answers)
+
     return render_template(
-        "frameworks/pre-qualification-questionnaire.html",
+        "frameworks/selection-questions.html",
+        form=form,
+        errors=[
+            {'input_name': k,
+             'question': v[0]} for k, v in form.errors.items()],
         **template_data
     ), 200
 
