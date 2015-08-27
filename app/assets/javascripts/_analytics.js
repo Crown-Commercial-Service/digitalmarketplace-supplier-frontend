49fd0e883b081a2cf630dd3f8894aebe74208ac6
Tom Byers <tombaromba@gmail.com>
2015-08-27 09:43:04 +0100

    Add link tracking (by event)

--- app/assets/javascripts/_analytics.js
+++ app/assets/javascripts/_analytics.js
@@ -10,14 +10,16 @@
   GOVUK.analytics.trackPageview();
 
   // Tracking for declaration pages
-  declarationPages = {
+  var declarationPages = {
     'firstPage': '/suppliers/frameworks/g-cloud-7/declaration/g_cloud_7_essentials',
     'lastPage': '/suppliers/frameworks/g-cloud-7/declaration/grounds_for_discretionary_exclusion'
   };
-  declarationVirtualPageViews = {
+  var declarationVirtualPageViews = {
     'firstQuestion': declarationPages.firstPage + '/interactions/accept-terms-of-participation',
     'makeDeclarationButton': declarationPages.lastPage + '/interactions/make-declaration'
   };
+  var externalLinkSelector = 'a[href^="https"]:not(a[href*="' + currentHost + '"])';
+  var pathName = window.location.pathname
 
   var registerFirstQuestionInteraction = function (e) {
     var $target;
@@ -34,15 +36,37 @@
   };
 
   var registerDeclarationMade = function (e) {
-    if (window.location.pathname === declarationPages.lastPage) {
+    if (pathName === declarationPages.lastPage) {
       GOVUK.analytics.trackPageview(declarationVirtualPageViews.makeDeclarationButton);
     }
   }
 
-  if (window.location.pathname === declarationPages.firstPage) {
+  var registerLinkClick = function (e) {
+    var $target = $(e.target),
+        href = $target.prop('href'),
+        category = 'internal-link',
+        action = $target.text(),
+        fileTypesRegExp = /\.(pdf|pda|odt|ods|odp)$/;
+
+    /* 
+       File type matching based on those in:
+       https://github.com/alphagov/digitalmarketplace-utils/blob/8251d45f47593bd73c5e3b993e1734b5ee505b4b/dmutils/documents.py#L105
+    */
+    if (href.match(fileTypesRegExp) !== null) { // download link
+      category = 'download';
+    }
+    else if (href.match(/^(https|http){1}/) !== null) { // external link
+      category = 'external-link';
+    }
+    GOVUK.analytics.trackEvent(category, action);
+  };
+
+  if (pathName === declarationPages.firstPage) {
     $('body').on('click', 'form.supplier-declaration fieldset:eq(0) label.selection-button', registerFirstQuestionInteraction);
   }
-  if (window.location.pathname === declarationPages.lastPage) {
+  if (pathName === declarationPages.lastPage) {
     $('form.supplier-declaration .button-save').on('click', registerDeclarationMade);
   }
+
+  $('body').on('click', 'a', registerLinkClick);
 })();
