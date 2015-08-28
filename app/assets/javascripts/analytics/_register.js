657f697825d038ec5a9ce05843033a3b2a500ec1
Tom Byers <tombaromba@gmail.com>
2015-08-28 11:57:22 +0100

    Switch tracked domain from beta to live

--- app/assets/javascripts/analytics/_register.js
+++ app/assets/javascripts/analytics/_register.js
@@ -5,7 +5,7 @@
   root.GOVUK.GDM.analytics = {
     'register': function () {
       GOVUK.Analytics.load();
-      var cookieDomain = (root.document.domain === 'www.beta.digitalmarketplace.service.gov.uk') ? '.digitalmarketplace.service.gov.uk' : root.document.domain;
+      var cookieDomain = (root.document.domain === 'www.digitalmarketplace.service.gov.uk') ? '.digitalmarketplace.service.gov.uk' : root.document.domain;
       var property = (root.document.domain === 'www.digitalmarketplace.service.gov.uk') ? 'UA-49258698-1' : 'UA-49258698-3';
       GOVUK.analytics = new GOVUK.Analytics({
         universalId: property,
