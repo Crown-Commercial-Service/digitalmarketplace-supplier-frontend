455bed869dbad71b20b005865478723b64f46a15
Alexey Bezhan <al@fueledbylemons.com>
2015-09-01 12:14:48 +0100

    Add CSRF error handler with log in redirect
    If a session expires and the first user action is to POST a form
    CSRF check fails to find the session token and shows an error.
    
    Since CSRF is triggered before the login_required decorator the user
    gets a CSRF error, even though the problem was caused by the expired
    session.
    
    To fix this we use a custom CSRF error handler that checks if session
    was expired and shows a log in page instead of CSRF error response.
    
    Since CSRF middleware stores a new token in session before error handler
    is called, we need to check for `user_id` key instead of checking if the
    session is empty.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import re
 
 from datetime import timedelta
-from flask import Flask, request, redirect, session, Markup
+from flask import Flask, request, redirect, session, Markup, abort
 from flask_login import LoginManager
 from flask_wtf.csrf import CsrfProtect
 
@@ -47,6 +47,21 @@ def create_app(config_name):
     main_blueprint.config = application.config.copy()
 
     csrf.init_app(application)
+
+    @csrf.error_handler
+    def csrf_handler(reason):
+        if 'user_id' not in session:
+            application.logger.info(
+                u'csrf.session_expired: Redirecting user to log in page'
+            )
+
+            return application.login_manager.unauthorized()
+
+        application.logger.info(
+            u'csrf.invalid_token: Aborting request, user_id: %s', session['user_id']
+        )
+
+        abort(400, reason)
 
     @application.before_request
     def remove_trailing_slash():
