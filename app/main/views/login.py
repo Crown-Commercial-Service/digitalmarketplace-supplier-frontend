7f7cd655ca1618f5a91f858c12add761baa226c4
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-02-01 22:26:51 +0000

    Remove routes and templates for logging in and resetting password
    These have all moved to the buyer app now, so need to be removed from here.

--- app/main/views/login.py
+++ app/main/views/login.py
@@ -3,207 +3,23 @@ import six
 from flask_login import login_required, current_user
 from itsdangerous import BadSignature, SignatureExpired
 from datetime import datetime
-from flask import current_app, flash, redirect, render_template, url_for, request, abort
-from flask_login import logout_user, login_user
+from flask import current_app, flash, redirect, render_template, url_for, abort
+from flask_login import login_user
 
 from dmapiclient import HTTPError
 from dmapiclient.audit import AuditTypes
-from dmutils.user import user_has_role, User
+from dmutils.user import User
 from dmutils.formats import DATETIME_FORMAT
 from dmutils.email import send_email, generate_token, decode_token, MandrillException
 
 from .. import main
-from ..forms.auth_forms import LoginForm, EmailAddressForm, ChangePasswordForm, CreateUserForm
+from ..forms.auth_forms import EmailAddressForm, CreateUserForm
 from ..helpers import hash_email
 from ... import data_api_client
 
 
 ONE_DAY_IN_SECONDS = 86400
 SEVEN_DAYS_IN_SECONDS = 604800
-
-
-@main.route('/login', methods=["GET"])
-def render_login():
-    next_url = request.args.get('next')
-    if current_user.is_authenticated():
-        if next_url and next_url.startswith('/suppliers'):
-            return redirect(next_url)
-        return redirect(url_for('.dashboard'))
-    template_data = main.config['BASE_TEMPLATE_DATA']
-    return render_template(
-        "auth/login.html",
-        form=LoginForm(),
-        next=next_url,
-        **template_data), 200
-
-
-@main.route('/login', methods=["POST"])
-def process_login():
-    form = LoginForm()
-    next_url = request.args.get('next')
-    template_data = main.config['BASE_TEMPLATE_DATA']
-    if form.validate_on_submit():
-
-        user_json = data_api_client.authenticate_user(
-            form.email_address.data,
-            form.password.data)
-
-        if not user_has_role(user_json, 'supplier'):
-            current_app.logger.info(
-                "login.fail: failed to log in {email_hash}",
-                extra={'email_hash': hash_email(form.email_address.data)})
-            flash("no_account", "error")
-            return render_template(
-                "auth/login.html",
-                form=form,
-                next=next_url,
-                **template_data), 403
-
-        user = User.from_json(user_json)
-        login_user(user)
-        current_app.logger.info("login.success")
-        if next_url and next_url.startswith('/suppliers'):
-            return redirect(next_url)
-
-        return redirect(url_for('.dashboard'))
-
-    else:
-        return render_template(
-            "auth/login.html",
-            form=form,
-            next=next_url,
-            **template_data), 400
-
-
-@main.route('/logout', methods=["GET"])
-def logout():
-    logout_user()
-    return redirect(url_for('.render_login'))
-
-
-@main.route('/reset-password', methods=["GET"])
-def request_password_reset():
-    template_data = main.config['BASE_TEMPLATE_DATA']
-
-    return render_template("auth/request-password-reset.html",
-                           form=EmailAddressForm(),
-                           **template_data), 200
-
-
-@main.route('/reset-password', methods=["POST"])
-def send_reset_password_email():
-    form = EmailAddressForm()
-    if form.validate_on_submit():
-        email_address = form.email_address.data
-        user_json = data_api_client.get_user(email_address=email_address)
-
-        if user_json is not None:
-
-            user = User.from_json(user_json)
-
-            token = generate_token(
-                {
-                    "user": user.id,
-                    "email": user.email_address
-                },
-                current_app.config['SECRET_KEY'],
-                current_app.config['RESET_PASSWORD_SALT']
-            )
-
-            url = url_for('main.reset_password', token=token, _external=True)
-
-            email_body = render_template(
-                "emails/reset_password_email.html",
-                url=url,
-                locked=user.locked)
-
-            try:
-                send_email(
-                    user.email_address,
-                    email_body,
-                    current_app.config['DM_MANDRILL_API_KEY'],
-                    current_app.config['RESET_PASSWORD_EMAIL_SUBJECT'],
-                    current_app.config['RESET_PASSWORD_EMAIL_FROM'],
-                    current_app.config['RESET_PASSWORD_EMAIL_NAME'],
-                    ["password-resets"]
-                )
-            except MandrillException as e:
-                current_app.logger.error(
-                    "Password reset email failed to send. "
-                    "error {error} email_hash {email_hash}",
-                    extra={'error': six.text_type(e),
-                           'email_hash': hash_email(user.email_address)})
-                abort(503, "Failed to send password reset")
-
-            message = "login.reset-email.sent: " \
-                      "Sending password reset email for supplier %d (%s)"
-
-            params = dict(
-                supplier_id=user.supplier_id,
-                email_hash=hash_email(user.email_address))
-            current_app.logger.info(
-                "login.reset-email.sent: Sending password reset email for "
-                "supplier_id {supplier_id} email_hash {email_hash}",
-                extra={'supplier_id': user.supplier_id,
-                       'email_hash': hash_email(user.email_address)})
-        else:
-            current_app.logger.info(
-                "login.reset-email.invalid-email: "
-                "Password reset request for invalid supplier email {email_hash}",
-                extra={'email_hash': hash_email(email_address)})
-
-        flash('email_sent')
-        return redirect(url_for('.request_password_reset'))
-    else:
-        template_data = main.config['BASE_TEMPLATE_DATA']
-        return render_template("auth/request-password-reset.html",
-                               form=form,
-                               **template_data), 400
-
-
-@main.route('/reset-password/<token>', methods=["GET"])
-def reset_password(token):
-    decoded = decode_password_reset_token(token)
-    if not decoded:
-        return redirect(url_for('.request_password_reset'))
-
-    email_address = decoded["email"]
-
-    template_data = main.config['BASE_TEMPLATE_DATA']
-    return render_template("auth/reset-password.html",
-                           email_address=email_address,
-                           form=ChangePasswordForm(),
-                           token=token,
-                           **template_data), 200
-
-
-@main.route('/reset-password/<token>', methods=["POST"])
-def update_password(token):
-    form = ChangePasswordForm()
-    decoded = decode_password_reset_token(token)
-    if not decoded:
-        return redirect(url_for('.request_password_reset'))
-
-    user_id = decoded["user"]
-    email_address = decoded["email"]
-    password = form.password.data
-
-    if form.validate_on_submit():
-        if data_api_client.update_user_password(user_id, password, email_address):
-            current_app.logger.info(
-                "User {user_id} successfully changed their password",
-                extra={'user_id': user_id})
-            flash('password_updated')
-        else:
-            flash('password_not_updated', 'error')
-        return redirect(url_for('.render_login'))
-    else:
-        template_data = main.config['BASE_TEMPLATE_DATA']
-        return render_template("auth/reset-password.html",
-                               email_address=email_address,
-                               form=form,
-                               token=token,
-                               **template_data), 400
 
 
 @main.route('/create-user/<string:encoded_token>', methods=["GET"])
