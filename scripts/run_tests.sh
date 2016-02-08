1cf2f6c940e368bc5e1acf42905e08c03d1bf59c
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2016-02-08 14:51:13 +0000

    Drive run scripts from the Makefile
    Now that we have a Makefile that captures these tasks these scripts
    should be driven by that. These scripts are being left here for people
    that are using them.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,29 +5,4 @@
 # NOTE: This script expects to be run from the project root with
 # ./scripts/run_tests.sh
 
-set -o pipefail
-
-function display_result {
-  RESULT=$1
-  EXIT_STATUS=$2
-  TEST=$3
-
-  if [ $RESULT -ne 0 ]; then
-    echo -e "\033[31m$TEST failed\033[0m"
-    exit $EXIT_STATUS
-  else
-    echo -e "\033[32m$TEST passed\033[0m"
-  fi
-}
-
-pep8 .
-display_result $? 2 "Code style check"
-
-npm run --silent frontend-build:production
-display_result $? 1 "Build of front end static assets"
-
-py.test $@
-display_result $? 3 "Python Unit tests"
-
-npm test
-display_result $? 4 "JavaScript Unit tests"
+make test
