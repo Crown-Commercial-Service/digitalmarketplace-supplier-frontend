4514e255863f80e3d6680d5c9bdf2b36e236aaf2
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-03-10 13:29:51 +0000

    Load G-Cloud 8 content

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -6,14 +6,21 @@ main = Blueprint('main', __name__)
 content_loader = ContentLoader('app/content')
 content_loader.load_manifest('g-cloud-6', 'services', 'edit_service')
 content_loader.load_messages('g-cloud-6', ['dates'])
+
 content_loader.load_manifest('g-cloud-7', 'services', 'edit_service')
 content_loader.load_manifest('g-cloud-7', 'services', 'edit_submission')
 content_loader.load_manifest('g-cloud-7', 'declaration', 'declaration')
 content_loader.load_messages('g-cloud-7', ['dates'])
+
 content_loader.load_manifest('digital-outcomes-and-specialists', 'declaration', 'declaration')
 content_loader.load_manifest('digital-outcomes-and-specialists', 'services', 'edit_submission')
 content_loader.load_manifest('digital-outcomes-and-specialists', 'brief-responses', 'edit_brief_response')
 content_loader.load_messages('digital-outcomes-and-specialists', ['dates'])
+
+content_loader.load_manifest('g-cloud-8', 'services', 'edit_service')
+content_loader.load_manifest('g-cloud-8', 'services', 'edit_submission')
+content_loader.load_manifest('g-cloud-8', 'declaration', 'declaration')
+content_loader.load_messages('g-cloud-8', ['dates'])
 
 
 @main.after_request
