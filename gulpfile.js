e96338472cf243ad1c5239a4a3982f183b5cf832
Chan, Grant <grant.chan@digital.gov.au>
2016-08-22 10:51:06 +1000

    Rebuilt using AU styling and build process.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ var include = require('gulp-include');
 var colours = require('colors/safe');
 var jasmine = require('gulp-jasmine-phantom');
 var sourcemaps = require('gulp-sourcemaps');
+var svg2png = require('gulp-svg2png');
+var bless = require('gulp-bless');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var plugins = require('gulp-load-plugins')();
 
 // Paths
 var environment;
@@ -21,6 +27,11 @@ var staticFolder = repoRoot + 'app/static';
 var govukTemplateFolder = repoRoot + 'bower_components/govuk_template';
 var govukTemplateAssetsFolder = govukTemplateFolder + '/assets';
 var govukTemplateLayoutsFolder = govukTemplateFolder + '/views/layouts';
+var paths = {
+    assetsDir: assetsFolder + '/images',
+    images: assetsFolder + '/images/icons/**/*.+(png|svg|jpg)',
+    outputAssets: staticFolder + '/images',
+};
 
 // JavaScript paths
 var jsSourceFile = assetsFolder + '/javascripts/application.js';
@@ -110,6 +121,11 @@ gulp.task('sass', function () {
     .pipe(
       sass(sassOptions[environment]))
         .on('error', logErrorAndExit)
+    .pipe(autoprefixer({
+        autoprefixer: {
+          browsers: ['last 2 versions', 'ie 8-10']
+        }
+    }))
     .pipe(gulp.dest(cssDistributionFolder));
 
   stream.on('end', function () {
@@ -117,6 +133,106 @@ gulp.task('sass', function () {
   });
 
   return stream;
+});
+
+gulp.task('sass:livereload', function () {
+  var stream = gulp.src(cssSourceGlob)
+    .pipe(filelog('Compressing SCSS files'))
+    .pipe(
+      sass(sassOptions[environment]))
+        .on('error', logErrorAndExit)
+    .pipe(autoprefixer({
+        autoprefixer: {
+          browsers: ['last 2 versions', 'ie 8-10']
+        }
+    }))
+    .pipe(gulp.dest(cssDistributionFolder))
+    .pipe(plugins.livereload());
+
+  stream.on('end', function () {
+    console.log('💾  Compressed CSS saved as .css files in ' + cssDistributionFolder);
+  });
+
+  return stream;
+});
+
+gulp.task('split', ['sass'], function(cb) {
+  var stream = gulp.src(cssDistributionFolder + '/application-ie8.css')
+      .pipe(filelog('Splitting CSS files'))
+      .pipe(bless({
+        imports: false
+      }))
+      .pipe(gulp.dest(cssDistributionFolder))
+      .pipe(rename(function(path) {
+        path.basename = path.basename.replace('blessed', 'part');
+      }));
+
+  stream.on('end', function() {
+    console.log('💾  Split CSS saved as .css files in ' + cssDistributionFolder);
+  });
+  cb();
+});
+
+gulp.task('rename-1', function() {
+  var fileTypes = [];
+  var complete = function (fileType) {
+    fileTypes.push(fileType);
+    if (fileTypes.length == 2) {
+      cb();
+    }
+  };
+  var logOutputFor = function (fileType) {
+    return function (err, paths) {
+      if (paths !== undefined) {
+        console.log('💥  Deleted the following ' + fileType + ' files:\n', paths.join('\n'));
+      }
+      complete(fileType);
+    };
+  };
+
+  var stream = gulp.src(cssDistributionFolder + '/application-ie8-blessed*')
+      .pipe(filelog('Renaming IE files 1'))
+      .pipe(rename(function(path) {
+        path.basename = ("application-ie8-part-1");
+      }))
+      .pipe(gulp.dest(cssDistributionFolder));
+
+  deleteFiles(cssDistributionFolder + '/application-ie8-blessed1.css', logOutputFor('CSS'));
+
+  stream.on('end', function() {
+    console.log('💾  Rename IE CSS saved as .css files in ' + cssDistributionFolder);
+  });
+});
+
+gulp.task('rename-2', function() {
+  var fileTypes = [];
+  var complete = function (fileType) {
+    fileTypes.push(fileType);
+    if (fileTypes.length == 2) {
+      cb();
+    }
+  };
+  var logOutputFor = function (fileType) {
+    return function (err, paths) {
+      if (paths !== undefined) {
+        console.log('💥  Deleted the following ' + fileType + ' files:\n', paths.join('\n'));
+      }
+      complete(fileType);
+    };
+  };
+
+  var stream = gulp.src(cssDistributionFolder + '/application-ie8.css')
+      .pipe(filelog('Renaming IE files 2'))
+      .pipe(rename(function(path) {
+        path.basename += ("-part-2");
+      }))
+      .pipe(gulp.dest(cssDistributionFolder));
+
+  deleteFiles(cssDistributionFolder + '/application-ie8.css', logOutputFor('CSS'));
+
+  stream.on('end', function() {
+    console.log('💾  Rename IE CSS saved as .css files in ' + cssDistributionFolder);
+  });
 });
 
 gulp.task('js', function () {
@@ -135,6 +251,31 @@ gulp.task('js', function () {
   });
 
   return stream;
+});
+
+gulp.task('js:livereload', function () {
+  var stream = gulp.src(jsSourceFile)
+    .pipe(filelog('Compressing JavaScript files'))
+    .pipe(include())
+    .pipe(sourcemaps.init())
+    .pipe(uglify(
+      uglifyOptions[environment]
+    ))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(jsDistributionFolder))
+    .pipe(plugins.livereload());
+
+  stream.on('end', function () {
+    console.log('💾 Compressed JavaScript saved as ' + jsDistributionFolder + '/' + jsDistributionFile);
+  });
+
+  return stream;
+});
+
+gulp.task('html:livereload', function () {
+  return gulp.src(repoRoot + 'app/templates/**/*.html')
+    .pipe(plugins.livereload());
+
 });
 
 function copyFactory(resourceName, sourceFolder, targetFolder) {
@@ -225,6 +366,26 @@ gulp.task(
 );
 
 gulp.task(
+  'copy:svg',
+  copyFactory(
+    "image assets from app to static folder",
+    assetsFolder + '/svg',
+    staticFolder + '/svg'
+  )
+);
+
+gulp.task('ui-kit.img', function() {
+  return gulp.src(paths.images)
+    .pipe(gulp.dest(paths.outputAssets));
+});
+
+gulp.task('svg2png', ['ui-kit.img'], function () {
+  return gulp.src(paths.assetsDir + '/icons/*.svg')
+    .pipe(svg2png())
+    .pipe(gulp.dest(paths.outputAssets + '/icons/'));
+});
+
+gulp.task(
   'copy:govuk_template',
   copyFactory(
     "GOV.UK template into app folder",
@@ -302,9 +463,29 @@ gulp.task(
   [
     'copy'
   ],
-  function() {
-    gulp.start('sass');
-    gulp.start('js');
+  function(cb) {
+    runSequence('sass',
+      'split',
+      'js',
+      'rename-1',
+      'rename-2',
+      cb);
+  }
+);
+
+gulp.task(
+  'compile:livereload',
+  [
+    'copy'
+  ],
+  function(cb) {
+    runSequence('sass:livereload',
+      'split',
+      'js:livereload',
+      'html:livereload',
+      'rename-1',
+      'rename-2',
+      cb);
   }
 );
 
@@ -314,4 +495,22 @@ gulp.task('build:development', ['set_environment_to_development', 'clean'], func
 
 gulp.task('build:production', ['set_environment_to_production', 'clean'], function () {
   gulp.start('compile');
+});
+
+gulp.task('build:livereload', ['set_environment_to_development', 'clean'], function () {
+  gulp.start('compile:livereload');
+});
+
+gulp.task('livereload', ['build:livereload'], function () {
+  plugins.livereload.listen();
+  var jsWatcher = gulp.watch([ assetsFolder + '/**/*.js' ], ['js:livereload']);
+  var cssWatcher = gulp.watch([ assetsFolder + '/**/*.scss' ], ['sass:livereload']);
+  var htmlWatcher = gulp.watch([ repoRoot + 'app/templates/**/*.html'], ['html:livereload']);
+  var notice = function (event) {
+    console.log('File ' + event.path + ' was ' + event.type + ' running tasks...');
+  };
+
+  cssWatcher.on('change', notice);
+  jsWatcher.on('change', notice);
+  htmlWatcher.on('change', notice);
 });
