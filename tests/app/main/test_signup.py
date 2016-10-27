d322f37cc5cd895b353fbba793372b2613e2cb07
matt-sm <mattysmith@gmail.com>
2016-10-27 14:34:49 +1100

    fix unit test

--- tests/app/main/test_signup.py
+++ tests/app/main/test_signup.py
@@ -1,5 +1,6 @@
 import mock
 from ..helpers import BaseApplicationTest
+from react.render_server import RenderedComponent
 
 
 class TestStartPage(BaseApplicationTest):
@@ -8,6 +9,9 @@ class TestStartPage(BaseApplicationTest):
 
     @mock.patch('app.main.views.signup.render_component')
     def test_start_page_renders(self, render_component):
+        render_component.return_value.get_props.return_value = {}
+        render_component.return_value.get_slug.return_value = 'slug'
+
         res = self.client.get(self.expand_path(
             '/signup'
             )
