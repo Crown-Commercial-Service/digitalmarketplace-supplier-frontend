f86b9300846355e8f8d722b61499684b137296ff
Paul Craig <paul_craig_16@hotmail.com>
2017-02-18 18:10:53 +0000

    Compensate for missing trailing semicolon
    The govuk_frontend_toolkit stuff uses different javascript
    formatting than us where they begin their files with
    semicolons but don't end files with them.
    
    Our JS does the opposite, and so concatenating the files
    results in a compilation bug somewhere in the middle.
    
    I've also changed the way everything is imported
    to more closely resemble the way the imports work in the
    buyer app.

--- app/assets/javascripts/_analytics.js
+++ app/assets/javascripts/_analytics.js
@@ -1,3 +1,8 @@
+//= include ../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/google-analytics-universal-tracker.js
+//= include ../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/analytics.js
+
+; // JavaScript in the govuk_frontend_toolkit doesn't have trailing semicolons
+
 //= include analytics/_register.js
 //= include analytics/_pageViews.js
 //= include analytics/_events.js
