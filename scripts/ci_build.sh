77ffc6a4a04301c34afe145d3cb4ad5b1a33c0c6
Alexander Sadleir <maxious@users.noreply.github.com>
2017-04-19 13:48:08 +1000

    Update ci_build.sh

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -4,5 +4,6 @@ npm install
 ./node_modules/.bin/pancake
 pip install -U pip
 pip install -U -r requirements.txt
+npm run frontend-install
 npm run frontend-build:production
 git log --pretty=format:'%h' -n 1 > version_label
