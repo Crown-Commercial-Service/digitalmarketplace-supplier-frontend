33160258cf735d7af41b827a4b832519a8712d05
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2017-07-20 16:10:55 +0100

    add initial default.nix

---
+++ default.nix
@@ -0,0 +1,46 @@
+{
+  pkgs ? import <nixpkgs> {},
+  pythonPackages ? pkgs.python36Packages,
+  forDev ? true
+}:
+{
+  digitalMarketplaceSupplierFrontendEnv = pkgs.stdenv.mkDerivation {
+    name = "digitalmarketplace-supplier-frontend-env";
+    buildInputs = [
+      pythonPackages.virtualenv
+      pkgs.nodejs
+      pkgs.libffi
+      pkgs.libyaml
+      # for `cryptography`
+      pkgs.openssl_1_1_0
+    ] ++ pkgs.stdenv.lib.optionals forDev ([
+        # for lxml
+        pkgs.libxml2
+        pkgs.libxslt
+        # for frontend tests
+        pkgs.phantomjs
+      ] ++ pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [
+        # for watchdog... not that it works on darwin yet anyway, but it will/would need them
+        pkgs.darwin.apple_sdk.frameworks.CoreServices
+        pkgs.darwin.cf-private
+      ]
+    );
+
+    hardeningDisable = pkgs.stdenv.lib.optionals pkgs.stdenv.isDarwin [ "format" ];
+
+    VIRTUALENV_ROOT = "venv${pythonPackages.python.pythonVersion}";
+    VIRTUAL_ENV_DISABLE_PROMPT = "1";
+    SOURCE_DATE_EPOCH = "315532800";
+
+    # if we don't have this, we get unicode troubles in a --pure nix-shell
+    LANG="en_GB.UTF-8";
+
+    shellHook = ''
+      if [ ! -e $VIRTUALENV_ROOT ]; then
+        ${pythonPackages.virtualenv}/bin/virtualenv $VIRTUALENV_ROOT
+      fi
+      source $VIRTUALENV_ROOT/bin/activate
+      make requirements${pkgs.stdenv.lib.optionalString forDev "-dev"}
+    '';
+  };
+}
