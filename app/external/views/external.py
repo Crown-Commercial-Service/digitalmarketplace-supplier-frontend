f69108970ca6951bf67724c6027088498a2f4614
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-08-18 13:06:06 +0100

    Redirect supplier opportunities dashboard
    The supplier opportunities dashboard has moved to the brief responses
    frontend app.
    
    This sets up a 301 redirect and moves the associated tests for the view.
    
    It also sets u a new external view so we can still link to the new view
    using `url_for`.

--- app/external/views/external.py
+++ app/external/views/external.py
@@ -6,3 +6,8 @@ external = Blueprint('external', __name__)
 @external.route('/suppliers/opportunities/<int:brief_id>/responses/result')
 def view_response_result(brief_id):
     raise NotImplementedError()
+
+
+@external.route('/suppliers/opportunities/frameworks/<framework_slug>')
+def opportunities_dashboard(framework_slug):
+    raise NotImplementedError()
--- app/main/views/frameworks.py
+++ app/main/views/frameworks.py
@@ -1193,22 +1193,4 @@ def view_contract_variation(framework_slug, variation_slug):
 @main.route('/frameworks/<framework_slug>/opportunities', methods=['GET'])
 @login_required
 def opportunities_dashboard(framework_slug):
-    try:
-        framework = data_api_client.get_framework(slug=framework_slug)['frameworks']
-        supplier_framework = data_api_client.get_supplier_framework_info(
-            supplier_id=current_user.supplier_id,
-            framework_slug=framework['slug']
-        )['frameworkInterest']
-    except APIError as e:
-        abort(e.status_code)
-    if not (framework['framework'] == 'digital-outcomes-and-specialists' and supplier_framework['onFramework']):
-        abort(404)
-    opportunities = data_api_client.find_brief_responses(
-        supplier_id=current_user.supplier_id, framework=framework_slug
-    )['briefResponses']
-
-    return render_template(
-        "suppliers/opportunities_dashboard.html",
-        framework=framework,
-        completed=opportunities
-    ), 200
+    return redirect("/suppliers/opportunities/frameworks/{}".format(framework_slug), code=301)
