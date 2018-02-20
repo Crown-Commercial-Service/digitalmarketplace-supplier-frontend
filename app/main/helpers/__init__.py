c1ba7385455ac10793beda20db98b3530428a8c8
George Lund <george.lund@digital.cabinet-office.gov.uk>
2018-02-20 09:49:30 +0000

    Move access control (by role) message into code, from the template.
     - this particular flash message is now in the dmutils lib.
    
    https://trello.com/c/H7upcsyQ/303-flash-messages-suppliers-app

--- app/main/helpers/__init__.py
+++ app/main/helpers/__init__.py
@@ -1,15 +1,10 @@
 from functools import wraps
 
-from flask import current_app, flash
-import flask_login
+from dmutils.access_control import require_login
 
 
 def login_required(func):
     @wraps(func)
-    @flask_login.login_required
     def decorated_view(*args, **kwargs):
-        if flask_login.current_user.is_authenticated() and flask_login.current_user.role != 'supplier':
-            flash('supplier-role-required', 'error')
-            return current_app.login_manager.unauthorized()
-        return func(*args, **kwargs)
+        return require_login(role="supplier") or func(*args, **kwargs)
     return decorated_view
