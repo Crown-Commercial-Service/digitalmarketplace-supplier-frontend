bc2df3289545cbb16cebb628fb1a821b13792d72
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2018-04-16 12:16:47 +0100

    Bump Flask-WTF to match utils requirement
    To prevents warning when installing requirements. This also triggers a
    bunch of deprecation warnings too. These are resolved by updating how we
    configure the CSRF errorhandler (it's not configure on the application
    object), CsrfProtect is renamed to CSRFProtect, and Form is renamed to
    FlaskForm

--- app/main/forms/auth_forms.py
+++ app/main/forms/auth_forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired
 
 from dmutils.forms import EmailField
 
 
-class EmailAddressForm(Form):
+class EmailAddressForm(FlaskForm):
     email_address = EmailField('Email address', validators=[
         InputRequired(message="Email address must be provided")
     ])
