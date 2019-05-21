d975917110e577c4780eab53254e60f553da62c6
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-05-21 16:53:22 +0100

    r+ not rw

--- run_nginx.py
+++ run_nginx.py
@@ -9,21 +9,21 @@ if environ.get("PROXY_AUTH_CREDENTIALS"):
         f.write("\n")
         f.write(environ["PROXY_AUTH_CREDENTIALS"])
 
-with open("/etc/nginx/nginx.conf", "rw") as f:
+with open("/etc/nginx/nginx.conf", "r+") as f:
     contents = f.read()
     contents.replace("{DM_APP_NAME}", environ.get("DM_APP_NAME"))
     f.seek(0)
     f.write(contents)
 
 if isfile("/etc/nginx/sites-enabled/api"):
-    with open("/etc/nginx/sites-enabled/api", "rw") as f:
+    with open("/etc/nginx/sites-enabled/api", "r+") as f:
         contents = f.read()
         contents.replace("{PORT}", environ.get("PORT", ""))
         f.seek(0)
         f.write(contents)
 
 if isfile("/etc/nginx/sites-enabled/frontend"):
-    with open("/etc/nginx/sites-enabled/frontend", "rw") as f:
+    with open("/etc/nginx/sites-enabled/frontend", "r+") as f:
         contents = f.read()
         contents.replace("{PORT}", environ.get("PORT", ""))
         f.seek(0)
