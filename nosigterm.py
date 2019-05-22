8d5646f95dafdf373b2b716af2904cb256773a1c
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-05-22 13:35:07 +0100

    use signal blocking instead of masking

--- nosigterm.py
+++ nosigterm.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 
-import signal
+import ctypes
 from os import execvp
 from sys import argv
 
-signal.signal(signal.SIGTERM, signal.SIG_IGN)
+# extracted from signal.h & friends, correct for x86_64 linux
+SIGTERM = ctypes.c_int(15)
+SIG_BLOCK = ctypes.c_int(0)
+sigset_t = ctypes.c_ulong * 16
+null_int = ctypes.c_int(0)
+
+libc = ctypes.CDLL(None)
+
+sigset = sigset_t()
+libc.sigemptyset(ctypes.byref(sigset))
+libc.sigaddset(ctypes.byref(sigset), SIGTERM)
+libc.sigprocmask(SIG_BLOCK, ctypes.byref(sigset), null_int)
+
 execvp(argv[1], argv[1:])
