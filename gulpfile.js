1cb403e5887b32dcf2575f76040a3f56685c5b9e
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-10-22 14:37:13 +0100

    Fixed gulp.js linting errors
    - Removed unnecessary semicolons
    - Used path.join() instead of `+` to join paths
    - Watch the digitalmarketplace-frameworks directory (bugfix)
    - Some small other tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,51 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var deleteFiles = require('del');
-var sass = require('gulp-sass');
-var filelog = require('gulp-filelog');
-var include = require('gulp-include');
-var colours = require('colors/safe');
-var jasmine = require('gulp-jasmine-phantom');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp')
+const uglify = require('gulp-uglify')
+const deleteFiles = require('del')
+const sass = require('gulp-sass')
+const filelog = require('gulp-filelog')
+const include = require('gulp-include')
+const colours = require('colors/safe')
+const jasmine = require('gulp-jasmine-phantom')
+const path = require('path')
+const sourcemaps = require('gulp-sourcemaps')
 
 // Paths
-var environment;
-var repoRoot = __dirname + '/';
-var npmRoot = repoRoot + 'node_modules';
-var govukCountryPickerDist = npmRoot + '/govuk-country-and-territory-autocomplete/dist';
-var govukToolkitRoot = npmRoot + '/govuk_frontend_toolkit';
-var govukElementsRoot = npmRoot + '/govuk-elements-sass';
-var govukFrontendRoot = npmRoot + '/govuk-frontend';
-var dmToolkitRoot = npmRoot + '/digitalmarketplace-frontend-toolkit/toolkit';
-var sspContentRoot = npmRoot + '/digitalmarketplace-frameworks';
-var assetsFolder = repoRoot + 'app/assets';
-var staticFolder = repoRoot + 'app/static';
-var govukTemplateFolder = repoRoot + 'node_modules/govuk_template';
-var govukTemplateAssetsFolder = govukTemplateFolder + '/assets';
-var govukTemplateLayoutsFolder = govukTemplateFolder + '/views/layouts';
-var govukFrontendFontsFolder = govukFrontendRoot + '/assets/fonts';
+let environment
+const repoRoot = path.join(__dirname)
+const npmRoot = path.join(repoRoot, 'node_modules')
+const govukCountryPickerDist = path.join(npmRoot, 'govuk-country-and-territory-autocomplete', 'dist')
+const govukToolkitRoot = path.join(npmRoot, 'govuk_frontend_toolkit')
+const govukElementsRoot = path.join(npmRoot, 'govuk-elements-sass')
+const govukFrontendRoot = path.join(npmRoot, 'govuk-frontend')
+const dmToolkitRoot = path.join(npmRoot, 'digitalmarketplace-frontend-toolkit', 'toolkit')
+const sspContentRoot = path.join(npmRoot, 'digitalmarketplace-frameworks')
+const assetsFolder = path.join(repoRoot, 'app', 'assets')
+const staticFolder = path.join(repoRoot, 'app', 'static')
+const govukTemplateFolder = path.join(repoRoot, 'node_modules', 'govuk_template')
+const govukTemplateAssetsFolder = path.join(govukTemplateFolder, 'assets')
+const govukTemplateLayoutsFolder = path.join(govukTemplateFolder, 'views', 'layouts')
+const govukFrontendFontsFolder = path.join(govukFrontendRoot, 'assets', 'fonts')
 
 // JavaScript paths
-var jsSourceFile = assetsFolder + '/javascripts/application.js';
-var jsDistributionFolder = staticFolder + '/javascripts';
-var jsDistributionFile = 'application.js';
-var jsPageSpecific = assetsFolder + '/javascripts/page_specific';
+const jsSourceFile = path.join(assetsFolder, 'javascripts', 'application.js')
+const jsDistributionFolder = path.join(staticFolder, 'javascripts')
+const jsDistributionFile = 'application.js'
+const jsPageSpecific = path.join(assetsFolder, 'javascripts', 'page_specific')
 
 // CSS paths
-var cssSourceGlob = assetsFolder + '/scss/application*.scss';
-var cssDistributionFolder = staticFolder + '/stylesheets';
+const cssSourceGlob = path.join(assetsFolder, 'scss', 'application*.scss')
+const cssDistributionFolder = path.join(staticFolder, 'stylesheets')
 
 // Configuration
-var sassOptions = {
+const sassOptions = {
   development: {
     outputStyle: 'expanded',
     lineNumbers: true,
     includePaths: [
-      assetsFolder + '/scss',
-      dmToolkitRoot + '/scss',
-      govukToolkitRoot + '/stylesheets',
-      govukElementsRoot + '/public/sass',
+      path.join(assetsFolder, 'scss'),
+      path.join(dmToolkitRoot, 'scss'),
+      path.join(govukToolkitRoot, 'stylesheets'),
+      path.join(govukElementsRoot, 'public', 'sass')
     ],
     sourceComments: true,
     errLogToConsole: true
@@ -53,15 +54,15 @@ var sassOptions = {
     outputStyle: 'compressed',
     lineNumbers: true,
     includePaths: [
-      assetsFolder + '/scss',
-      dmToolkitRoot + '/scss',
-      govukToolkitRoot + '/stylesheets',
-      govukElementsRoot + '/public/sass',
-    ],
-  },
-};
+      path.join(assetsFolder, 'scss'),
+      path.join(dmToolkitRoot, 'scss'),
+      path.join(govukToolkitRoot, 'stylesheets'),
+      path.join(govukElementsRoot, 'public', 'sass')
+    ]
+  }
+}
 
-var uglifyOptions = {
+const uglifyOptions = {
   development: {
     mangle: false,
     output: {
@@ -75,280 +76,274 @@ var uglifyOptions = {
   production: {
     mangle: true
   }
-};
+}
 
-var logErrorAndExit = function logErrorAndExit(err) {
-  var printError = function (type, message) {
-    console.log('gulp ' + colours.red('ERR! ') + type + ': ' + message);
-  };
+const logErrorAndExit = function logErrorAndExit (err) {
+  const printError = function (type, message) {
+    console.log('gulp ' + colours.red('ERR! ') + type + ': ' + message)
+  }
 
-  printError('message', err.message);
-  printError('file name', err.fileName);
-  printError('line number', err.lineNumber);
-  process.exit(1);
-
-};
+  printError('message', err.message)
+  printError('file name', err.fileName)
+  printError('line number', err.lineNumber)
+  process.exit(1)
+}
 
 gulp.task('clean', function (cb) {
-  var fileTypes = [];
-  var complete = function (fileType) {
-    fileTypes.push(fileType);
-    if (fileTypes.length == 2) {
-      cb();
+  var fileTypes = []
+  const complete = function (fileType) {
+    fileTypes.push(fileType)
+    if (fileTypes.length === 2) {
+      cb()
     }
-  };
-  var logOutputFor = function (fileType) {
-    return function (err, paths) {
+  }
+  const logOutputFor = function (fileType) {
+    return function (_, paths) {
       if (paths !== undefined) {
-        console.log('💥  Deleted the following ' + fileType + ' files:\n', paths.join('\n'));
+        console.log('💥  Deleted the following ' + fileType + ' files:\n', paths.join('\n'))
       }
-      complete(fileType);
-    };
-  };
+      complete(fileType)
+    }
+  }
 
-  deleteFiles(jsDistributionFolder + '/**/*', logOutputFor('JavaScript'));
-  deleteFiles(cssDistributionFolder + '/**/*', logOutputFor('CSS'));
-});
+  deleteFiles(path.join(jsDistributionFolder, '**', '*'), logOutputFor('JavaScript'))
+  deleteFiles(path.join(cssDistributionFolder, '**', '*'), logOutputFor('CSS'))
+})
 
 gulp.task('sass', function () {
-  var stream = gulp.src(cssSourceGlob)
+  const stream = gulp.src(cssSourceGlob)
     .pipe(filelog('Compressing SCSS files'))
     .pipe(
       sass(sassOptions[environment]))
-        .on('error', logErrorAndExit)
-    .pipe(gulp.dest(cssDistributionFolder));
+    .on('error', logErrorAndExit)
+    .pipe(gulp.dest(cssDistributionFolder))
 
   stream.on('end', function () {
-    console.log('💾  Compressed CSS saved as .css files in ' + cssDistributionFolder);
-  });
+    console.log('💾  Compressed CSS saved as .css files in ' + cssDistributionFolder)
+  })
 
-  return stream;
-});
+  return stream
+})
 
 gulp.task('js', function () {
-  var stream = gulp.src(jsSourceFile)
+  const stream = gulp.src(jsSourceFile)
     .pipe(filelog('Compressing JavaScript files'))
-    .pipe(include({'hardFail': true}))
+    .pipe(include({ hardFail: true }))
     .pipe(sourcemaps.init())
     .pipe(uglify(
       uglifyOptions[environment]
     ))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest(jsDistributionFolder));
+    .pipe(sourcemaps.write(path.join('.', 'maps')))
+    .pipe(gulp.dest(jsDistributionFolder))
 
   stream.on('end', function () {
-    console.log('💾 Compressed JavaScript saved as ' + jsDistributionFolder + '/' + jsDistributionFile);
-  });
+    console.log('💾 Compressed JavaScript saved as ' + path.join(jsDistributionFolder, jsDistributionFile))
+  })
 
-  return stream;
-});
+  return stream
+})
 
-function copyFactory(resourceName, sourceFolder, targetFolder) {
-
-  return function() {
-
+function copyFactory (resourceName, sourceFolder, targetFolder) {
+  return function () {
     return gulp
-      .src(sourceFolder + "/**/*", { base: sourceFolder })
+      .src(path.join(sourceFolder, '**', '*'), { base: sourceFolder })
       .pipe(gulp.dest(targetFolder))
       .on('end', function () {
-        console.log('📂  Copied ' + resourceName);
-      });
-
-  };
-
+        console.log('📂  Copied ' + resourceName)
+      })
+  }
 }
 
-function copyFiletypeFactory(resourceName, sourceFolder, sourceFileExtension, targetFolder) {
-
-  return function() {
+function copyFiletypeFactory (resourceName, sourceFolder, sourceFileExtension, targetFolder) {
+  return function () {
     return gulp
-      .src(sourceFolder + "/**/*." + sourceFileExtension, { base: sourceFolder })
+      .src(path.join(sourceFolder, '**', '*.' + sourceFileExtension), { base: sourceFolder })
       .pipe(gulp.dest(targetFolder))
       .on('end', function () {
-        console.log('📂  Copied ' + resourceName );
-      });
-
-  };
-
+        console.log('📂  Copied ' + resourceName)
+      })
+  }
 }
 
 gulp.task(
   'copy:template_assets:stylesheets',
   copyFactory(
-    "GOV.UK template stylesheets",
-    govukTemplateAssetsFolder + '/stylesheets',
-    staticFolder + '/stylesheets'
+    'GOV.UK template stylesheets',
+    path.join(govukTemplateAssetsFolder, 'stylesheets'),
+    path.join(staticFolder, 'stylesheets')
   )
-);
+)
 
 gulp.task(
   'copy:template_assets:images',
   copyFactory(
-    "GOV.UK template images",
-    govukTemplateAssetsFolder + '/images',
-    staticFolder + '/images'
+    'GOV.UK template images',
+    path.join(govukTemplateAssetsFolder, 'images'),
+    path.join(staticFolder, 'images')
   )
-);
+)
 
 gulp.task(
   'copy:template_assets:javascripts',
   copyFactory(
     'GOV.UK template Javascript files',
-    govukTemplateAssetsFolder + '/javascripts',
-    staticFolder + '/javascripts'
+    path.join(govukTemplateAssetsFolder, 'javascripts'),
+    path.join(staticFolder, 'javascripts')
   )
-);
+)
 
 gulp.task(
   'copy:dm_toolkit_assets:stylesheets',
   copyFactory(
-    "stylesheets from the Digital Marketplace frontend toolkit",
-    dmToolkitRoot + '/scss',
-    'app/assets/scss/toolkit'
+    'stylesheets from the Digital Marketplace frontend toolkit',
+    path.join(dmToolkitRoot, 'scss'),
+    path.join('app', 'assets', 'scss', 'toolkit')
   )
-);
+)
 
 gulp.task(
   'copy:dm_toolkit_assets:images',
   copyFactory(
-    "images from the Digital Marketplace frontend toolkit",
-    dmToolkitRoot + '/images',
-    staticFolder + '/images'
+    'images from the Digital Marketplace frontend toolkit',
+    path.join(dmToolkitRoot, 'images'),
+    path.join(staticFolder, 'images')
   )
-);
+)
 
 gulp.task(
   'copy:govuk_toolkit_assets:images',
   copyFactory(
-    "images from the GOVUK frontend toolkit",
-    govukToolkitRoot + '/images',
-    staticFolder + '/images'
+    'images from the GOVUK frontend toolkit',
+    path.join(govukToolkitRoot, 'images'),
+    path.join(staticFolder, 'images')
   )
-);
+)
 
 gulp.task(
   'copy:dm_toolkit_assets:templates',
   copyFactory(
-    "templates from the Digital Marketplace frontend toolkit",
-    dmToolkitRoot + '/templates',
-    'app/templates/toolkit'
+    'templates from the Digital Marketplace frontend toolkit',
+    path.join(dmToolkitRoot, 'templates'),
+    path.join('app', 'templates', 'toolkit')
   )
-);
+)
 
 gulp.task(
   'copy:images',
   copyFactory(
-    "image assets from app to static folder",
-    assetsFolder + '/images',
-    staticFolder + '/images'
+    'image assets from app to static folder',
+    path.join(assetsFolder, 'images'),
+    path.join(staticFolder, 'images')
   )
-);
+)
 
 gulp.task(
   'copy:govuk_template',
   copyFactory(
-    "GOV.UK template into app folder",
+    'GOV.UK template into app folder',
     govukTemplateLayoutsFolder,
-    'app/templates/govuk'
+    path.join('app', 'templates', 'govuk')
   )
-);
+)
 
 gulp.task(
   'copy:frameworks',
   copyFactory(
-    "frameworks YAML into app folder",
-    sspContentRoot + '/frameworks', 'app/content/frameworks'
+    'frameworks YAML into app folder',
+    path.join(sspContentRoot, 'frameworks'),
+    path.join('app', 'content', 'frameworks')
   )
-);
+)
 
 gulp.task(
   'copy:govuk_frontend_assets:fonts',
   copyFactory(
-    "fonts from the GOVUK frontend assets",
+    'fonts from the GOVUK frontend assets',
     govukFrontendFontsFolder,
-    staticFolder + '/fonts'
+    path.join(staticFolder, 'fonts')
   )
-);
+)
 
 gulp.task(
   'copy:country_picker:jsons',
   copyFiletypeFactory(
-    "country picker jsons to static folder",
+    'country picker jsons to static folder',
     govukCountryPickerDist,
-    "json",
+    'json',
     staticFolder
   )
-);
+)
 
 gulp.task(
   'copy:country_picker:stylesheets',
   copyFiletypeFactory(
-    "country picker stylesheets to static folder",
+    'country picker stylesheets to static folder',
     govukCountryPickerDist,
-    "css",
+    'css',
     cssDistributionFolder
   )
-);
+)
 
 gulp.task(
   'copy:country_picker_package:javascripts',
   copyFiletypeFactory(
-    "country picker package javascripts to static folder",
+    'country picker package javascripts to static folder',
     govukCountryPickerDist,
-    "{js,js.map}",
+    '{js,js.map}',
     jsDistributionFolder
   )
-);
+)
 
 gulp.task(
   'copy:page_specific:javascripts',
   copyFactory(
-    "page specific javascript to static folder",
+    'page specific javascript to static folder',
     jsPageSpecific,
     jsDistributionFolder
   )
-);
+)
 
 gulp.task('test', function () {
-  var manifest = require(repoRoot + 'spec/javascripts/manifest.js').manifest;
+  const manifest = require(path.join(repoRoot, 'spec', 'javascripts', 'manifest.js')).manifest
 
   manifest.support = manifest.support.map(function (val) {
-    return val.replace(/^(\.\.\/){3}/, '');
-  });
+    return val.replace(/^(\.\.\/){3}/, '')
+  })
   manifest.test = manifest.test.map(function (val) {
-    return val.replace(/^\.\.\//, 'spec/javascripts/');
-  });
+    return val.replace(/^\.\.\//, 'spec/javascripts/')
+  })
 
   return gulp.src(manifest.test)
     .pipe(jasmine({
-      'jasmine': '2.0',
-      'integration': true,
-      'abortOnFail': true,
-      'vendor': manifest.support
-    }));
-});
+      jasmine: '2.0',
+      integration: true,
+      abortOnFail: true,
+      vendor: manifest.support
+    }))
+})
 
 gulp.task('watch', ['build:development'], function () {
-  var jsWatcher = gulp.watch([ assetsFolder + '/**/*.js' ], ['js']);
-  var cssWatcher = gulp.watch([ assetsFolder + '/**/*.scss' ], ['sass']);
-  var dmWatcher = gulp.watch([ npmRoot + '/digitalmarketplace-frameworks/**' ], ['copy:frameworks']);
-  var notice = function (event) {
-    console.log('File ' + event.path + ' was ' + event.type + ' running tasks...');
-  };
+  const jsWatcher = gulp.watch([path.join(assetsFolder, '**', '*.js')], ['js'])
+  const cssWatcher = gulp.watch([path.join(assetsFolder, '**', '*.scss')], ['sass'])
+  const dmWatcher = gulp.watch([path.join(npmRoot, 'digitalmarketplace-frameworks', '**')], ['copy:frameworks'])
+  const notice = function (event) {
+    console.log('File ' + event.path + ' was ' + event.type + ' running tasks...')
+  }
 
-  cssWatcher.on('change', notice);
-  jsWatcher.on('change', notice);
-});
+  cssWatcher.on('change', notice)
+  jsWatcher.on('change', notice)
+  dmWatcher.on('change', notice)
+})
 
 gulp.task('set_environment_to_development', function (cb) {
-  environment = 'development';
-  cb();
-});
+  environment = 'development'
+  cb()
+})
 
 gulp.task('set_environment_to_production', function (cb) {
-  environment = 'production';
-  cb();
-});
+  environment = 'production'
+  cb()
+})
 
 gulp.task(
   'copy',
@@ -369,23 +364,23 @@ gulp.task(
     'copy:page_specific:javascripts',
     'copy:govuk_frontend_assets:fonts'
   ]
-);
+)
 
 gulp.task(
   'compile',
   [
     'copy'
   ],
-  function() {
-    gulp.start('sass');
-    gulp.start('js');
+  function () {
+    gulp.start('sass')
+    gulp.start('js')
   }
-);
+)
 
 gulp.task('build:development', ['set_environment_to_development', 'clean'], function () {
-  gulp.start('compile');
-});
+  gulp.start('compile')
+})
 
 gulp.task('build:production', ['set_environment_to_production', 'clean'], function () {
-  gulp.start('compile');
-});
+  gulp.start('compile')
+})
