38ae987348d36d4f61380bd29bc8f6bce3607bd1
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-11-04 18:12:45 +0000

    Add JavaScript from GOV.UK Frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,14 @@
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/show-hide-content.js
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/user-research-consent-banner.js
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/report-a-problem.js
+//= require ../../../node_modules/digitalmarketplace-govuk-frontend/govuk-frontend/all.js
 //= require _analytics.js
 //= require _selection-buttons.js
 //= require _stick-at-top-when-scrolling.js
 //= require _stop-scrolling-at-footer.js
 //= require category-picker.js
+
+GOVUKFrontend.initAll();
 
 (function(GOVUK, GDM) {
 
