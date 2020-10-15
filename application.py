01b78809e89fa76ffb66dc78ca07bad2f74f5927
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-10-15 11:33:02 +0100

    Remove Flask-Script manager from application.py
    With built in Flask CLI this is no longer needed.

--- application.py
+++ application.py
@@ -1,25 +1,6 @@
-#!/usr/bin/env python
-
 import os
 
-# import jinja2
-
 from app import create_app
-from dmutils import init_manager
 
-application = create_app(
-    os.getenv('DM_ENVIRONMENT') or 'development'
-)
-application.jinja_options = {
-    'extensions': [
-        'jinja2.ext.with_'
-    ]
-}
 
-# Sadly can't enable this yet, but we should work towards it...
-# application.jinja_env.undefined = jinja2.StrictUndefined
-
-manager = init_manager(application, 5003, ['./app/content/frameworks'])
-
-if __name__ == '__main__':
-    manager.run()
+application = create_app(os.getenv("DM_ENVIRONMENT") or "development")
