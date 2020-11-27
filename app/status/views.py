7e9af8e157e06c97c8b5a47e5fd66ae42060651e
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2020-11-27 13:14:02 +0000

    Stop redefining 'status'
    As picked up by mypy

--- app/status/views.py
+++ app/status/views.py
@@ -6,7 +6,7 @@ from dmutils.status import get_app_status
 
 
 @status.route('/_status')
-def status():
+def show_status():
     return get_app_status(data_api_client=data_api_client,
                           search_api_client=None,
                           ignore_dependencies='ignore-dependencies' in request.args)
